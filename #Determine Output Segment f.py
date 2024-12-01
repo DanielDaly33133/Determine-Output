d94d0e1d5b7bef889e4ca99918e0c213f6a1d446
DanielDaly33133 <ddaly33133@gmail.com>
2024-12-01 12:50:35 -0500

    commit for segment f

---
+++ #Determine Output Segment f.py
@@ -0,0 +1,14 @@
+#Determine Output Segment f
+
+#Variables
+p = 2
+q = 4
+
+#While loop
+while (p < q):
+        print("Adios")
+        r = 1
+        while (r < q):
+            print("Adios")
+            r = r + 1
+            p = p + 1
